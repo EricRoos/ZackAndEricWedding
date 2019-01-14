4f5d2f2dd66f4c3190a69ece4b912c119d8878fe
Eric Roos <ericroos13@gmail.com>
2019-01-14 15:19:28 -0600

    make gallery load via json and ajax

--- libs/gallery-grid/gallery-grid.js
+++ libs/gallery-grid/gallery-grid.js
@@ -10,23 +10,38 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 **/
+
+function buildGalleryImageSection(imgData){
+  let classes = imgData.categories.map(function(category){
+    return "gallery-"+category;
+  }).join(" ");
+  var out = ""
+  out += '<div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter '+ classes +'">'
+  out += '<a href="img/gallery/gallery-image-01.jpg" class="mfp-image" title="Gallery image title">'
+  out += '<img src="img/gallery/gallery-image-01.jpg" class="img-responsive" alt="Gallery image">'
+  out += '</a></div>'
+  console.log(out);
+  return out;
+}
 $(document).ready(function(){
-
-    $(".filter-button").click(function(){
+  $.get("/gallery.json", function(data){
+    data.forEach(function(imgData){
+      console.log(imgData);
+      $(".gallery-popup").append(buildGalleryImageSection(imgData));
+      $(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
-        
         if(value == "all"){
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
+          //$('.filter').removeClass('hidden');
+          $('.filter').show('1000');
         }else{
-            // $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-            // $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
+          // $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+          // $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+          $(".filter").not('.'+value).hide('3000');
+          $('.filter').filter('.'+value).show('3000');
         }
-
         $(".filter-button").removeClass("active");
         $(this).addClass("active");
-
+      });
     });
+  });
 });
