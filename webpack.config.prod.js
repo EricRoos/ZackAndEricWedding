6d64186e77d94f297997cf2e7bdc78e4d5598677
Eric Roos <ericroos13@gmail.com>
2019-01-18 11:27:06 -0600

    update with prod build

---
+++ webpack.config.prod.js
@@ -0,0 +1,61 @@
+var path = require('path');
+var webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle-[hash].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['./dist'], {}),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: false,
+      template: './index.html'  
+    }),
+    new CopyWebpackPlugin([
+      './img/**/*',
+      './*.json'
+    ]),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "style.[hash].css",
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg|eot|woff2|ttf|woff)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=images/[name].[ext]',
+        ]
+      },
+      {
+        test: /\.css$/,
+        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
